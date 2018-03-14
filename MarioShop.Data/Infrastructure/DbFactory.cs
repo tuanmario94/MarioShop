ca953515e464ad5c0746d7234a688791d0ec24df
DESKTOP-6TMTT1H\TuanMario <tuanmario94@gmail.com>
2018-03-15 00:02:50 +0700

    add entity framework code first for data

---
+++ MarioShop.Data/Infrastructure/DbFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarioShop.Data.Infrastructure
+{
+    public class DbFactory : Disposable, IDbFactory
+    {
+        private MarioShopDbContext dbContext;
+        public MarioShopDbContext Init()
+        {
+            if (dbContext == null)
+            {
+                dbContext = new MarioShopDbContext();
+            }
+            return dbContext;
+        }
+        protected override void DisposeCore()
+        {
+            if (dbContext != null)
+                dbContext.Dispose();
+        }
+    }
+}
