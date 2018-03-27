92018c58a838e102da7b6ae28558d7fbcaf894f3
DESKTOP-6TMTT1H\TuanMario <tuanmario94@gmail.com>
2018-03-28 02:04:33 +0700

    automapper for web

--- MarioShop.Web/Api/PostCategoryController.cs
+++ MarioShop.Web/Api/PostCategoryController.cs
@@ -1,5 +1,9 @@
-﻿using MarioShop.Service;
+﻿using AutoMapper;
+using MarioShop.Model.Models;
+using MarioShop.Service;
 using MarioShop.Web.Infrastructure.Core;
+using MarioShop.Web.Infrastructure.Extensions;
+using MarioShop.Web.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,103 +13,100 @@ using System.Web.Http;
 
 namespace MarioShop.Web.Api
 {
-    public class PostCategoryController : ApiController
+    [RoutePrefix("api/postcategory")]
+    public class PostCategoryController : ApiControllerBase
     {
-        [RoutePrefix("api/postcategory")]
-        [Authorize]
-        public class PostCategoryController : ApiControllerBase
+        IPostCategoryService _postCategoryService;
+
+        public PostCategoryController(IErrorService errorService, IPostCategoryService postCategoryService) :
+            base(errorService)
         {
-            IPostCategoryService _postCategoryService;
+            this._postCategoryService = postCategoryService;
+        }
 
-            public PostCategoryController(IErrorService errorService, IPostCategoryService postCategoryService) :
-                base(errorService)
+        [Route("getall")]
+        public HttpResponseMessage Get(HttpRequestMessage request)
+        {
+            return CreateHttpResponse(request, () =>
             {
-                this._postCategoryService = postCategoryService;
-            }
+                var listCategory = _postCategoryService.GetAll();
 
-            [Route("getall")]
-            public HttpResponseMessage Get(HttpRequestMessage request)
+                var listPostCategoryVm = Mapper.Map<List<PostCategoryViewModel>>(listCategory);
+
+                HttpResponseMessage response = request.CreateResponse(HttpStatusCode.OK, listPostCategoryVm);
+
+                return response;
+            });
+        }
+
+        [Route("add")]
+        public HttpResponseMessage Post(HttpRequestMessage request, PostCategoryViewModel postCategoryVm)
+        {
+            return CreateHttpResponse(request, () =>
             {
-                return CreateHttpResponse(request, () =>
+                HttpResponseMessage response = null;
+                if (ModelState.IsValid)
                 {
-                    var listCategory = _postCategoryService.GetAll();
-
-                    var listPostCategoryVm = Mapper.Map<List<PostCategoryViewModel>>(listCategory);
-
-                    HttpResponseMessage response = request.CreateResponse(HttpStatusCode.OK, listPostCategoryVm);
-
-                    return response;
-                });
-            }
-
-            [Route("add")]
-            public HttpResponseMessage Post(HttpRequestMessage request, PostCategoryViewModel postCategoryVm)
-            {
-                return CreateHttpResponse(request, () =>
+                    request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+                }
+                else
                 {
-                    HttpResponseMessage response = null;
-                    if (ModelState.IsValid)
-                    {
-                        request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
-                    }
-                    else
-                    {
-                        PostCategory newPostCategory = new PostCategory();
-                        newPostCategory.UpdatePostCategory(postCategoryVm);
+                    PostCategory newPostCategory = new PostCategory();
+                    newPostCategory.UpdatePostCategory(postCategoryVm);
 
-                        var category = _postCategoryService.Add(newPostCategory);
-                        _postCategoryService.Save();
+                    var category = _postCategoryService.Add(newPostCategory);
+                    _postCategoryService.Save();
 
-                        response = request.CreateResponse(HttpStatusCode.Created, category);
+                    response = request.CreateResponse(HttpStatusCode.Created, category);
 
-                    }
-                    return response;
-                });
-            }
+                }
+                return response;
+            });
+        }
 
-            [Route("update")]
-            public HttpResponseMessage Put(HttpRequestMessage request, PostCategoryViewModel postCategoryVm)
+        [Route("update")]
+        public HttpResponseMessage Put(HttpRequestMessage request, PostCategoryViewModel postCategoryVm)
+        {
+            return CreateHttpResponse(request, () =>
             {
-                return CreateHttpResponse(request, () =>
+                HttpResponseMessage response = null;
+                if (ModelState.IsValid)
                 {
-                    HttpResponseMessage response = null;
-                    if (ModelState.IsValid)
-                    {
-                        request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
-                    }
-                    else
-                    {
-                        var postCategoryDb = _postCategoryService.GetById(postCategoryVm.ID);
-                        postCategoryDb.UpdatePostCategory(postCategoryVm);
-                        _postCategoryService.Update(postCategoryDb);
-                        _postCategoryService.Save();
+                    request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+                }
+                else
+                {
+                    var postCategoryDb = _postCategoryService.GetById(postCategoryVm.ID);
+                    postCategoryDb.UpdatePostCategory(postCategoryVm);
+                    _postCategoryService.Update(postCategoryDb);
+                    _postCategoryService.Save();
 
-                        response = request.CreateResponse(HttpStatusCode.OK);
+                    response = request.CreateResponse(HttpStatusCode.OK);
 
-                    }
-                    return response;
-                });
-            }
+                }
+                return response;
+            });
+        }
 
-            public HttpResponseMessage Delete(HttpRequestMessage request, int id)
+        public HttpResponseMessage Delete(HttpRequestMessage request, int id)
+        {
+            return CreateHttpResponse(request, () =>
             {
-                return CreateHttpResponse(request, () =>
+                HttpResponseMessage response = null;
+                if (ModelState.IsValid)
                 {
-                    HttpResponseMessage response = null;
-                    if (ModelState.IsValid)
-                    {
-                        request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
-                    }
-                    else
-                    {
-                        _postCategoryService.Delete(id);
-                        _postCategoryService.Save();
+                    request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+                }
+                else
+                {
+                    _postCategoryService.Delete(id);
+                    _postCategoryService.Save();
 
-                        response = request.CreateResponse(HttpStatusCode.OK);
+                    response = request.CreateResponse(HttpStatusCode.OK);
 
-                    }
-                    return response;
-                });
-            }
+                }
+                return response;
+            });
         }
     }
+}
