e2af3d1265f50b46211645ba20be39d2c2bbc852
DESKTOP-6TMTT1H\TuanMario <tuanmario94@gmail.com>
2018-03-31 10:52:06 +0700

    What is scope and root scope

---
+++ MarioShop.Web/Scripts/spa/app.js
@@ -0,0 +1,18 @@
+﻿/// <reference path="../plugins/bower-angular-master/angular.js" />
+
+var myApp = angular.module('myModule', []);
+
+myApp.controller("studentController", studentController);
+myApp.controller("teacherController", teacherController);
+myApp.controller("schoolController", schoolController);
+//declare
+function studentController($scope) {
+    //$scope.message = "This is my message from studentController";
+}
+function teacherController($scope) {
+    //$scope.message = "This is my message from teacherController";
+}
+
+function schoolController($scope) {
+    $scope.message = "Announce from school!";
+}
