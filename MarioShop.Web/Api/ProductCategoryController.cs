2a6792913a435f32569f1faefbf760ca8874995c
DESKTOP-6TMTT1H\TuanMario <tuanmario94@gmail.com>
2018-04-03 22:06:56 +0700

    delete mutil record

--- MarioShop.Web/Api/ProductCategoryController.cs
+++ MarioShop.Web/Api/ProductCategoryController.cs
@@ -169,5 +169,33 @@ namespace MarioShop.Web.Api
             });
         }
 
+        [Route("deletemulti")]
+        [HttpDelete]
+        [AllowAnonymous]
+        public HttpResponseMessage DeleteMulti(HttpRequestMessage request, string checkedProductCategories)
+        {
+            return CreateHttpResponse(request, () =>
+            {
+                HttpResponseMessage response = null;
+                if (!ModelState.IsValid)
+                {
+                    response = request.CreateResponse(HttpStatusCode.BadRequest, ModelState);
+                }
+                else
+                {
+                    var listProductCategory = new JavaScriptSerializer().Deserialize<List<int>>(checkedProductCategories);
+                    foreach (var item in listProductCategory)
+                    {
+                        _productCategoryService.Delete(item);
+                    }
+
+                    _productCategoryService.Save();
+
+                    response = request.CreateResponse(HttpStatusCode.OK, listProductCategory.Count);
+                }
+
+                return response;
+            });
+        }
     }
 }
